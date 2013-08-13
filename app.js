5413d746255e5826810fcb27138bd449265aabf8
Madison May <madison.may@students.olin.edu>
2013-08-13 07:17:38 -0400

    Add rem-js

--- app.js
+++ app.js
@@ -4,7 +4,8 @@
  */
 
 var express = require('express')
-  , routes = require('./routes');
+  , routes = require('./routes')
+  , rem = require('rem');
 
 var app = module.exports = express.createServer();
 

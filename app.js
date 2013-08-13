e7e9934ffa28eb443f31e78dd60108d1bfe6d315
Madison May <madison.may@students.olin.edu>
2013-08-13 07:19:47 -0400

    Port var added

--- app.js
+++ app.js
@@ -3,9 +3,9 @@
  * Module dependencies.
  */
 
-var express = require('express')
-  , routes = require('./routes')
-  , rem = require('rem');
+var express = require('express'),
+    routes = require('./routes'),
+    rem = require('rem');
 
 var app = module.exports = express.createServer();
 
@@ -14,6 +14,10 @@ var app = module.exports = express.createServer();
 app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(express.cookieParser());
+  app.use(express.session({
+    secret: "some arbitrary secret"
+  }));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
   app.use(app.router);
@@ -28,10 +32,12 @@ app.configure('production', function(){
   app.use(express.errorHandler());
 });
 
+app.set('port', process.env.PORT || 3000);
+
 // Routes
 
 app.get('/', routes.index);
 
-app.listen(3000, function(){
+app.listen(app.get('port'), function(){
   console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
 });

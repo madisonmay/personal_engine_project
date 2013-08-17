22dbf7964d5ad4face5c1c1c6637f6c3ef509a42
Madison May <madison.may@students.olin.edu>
2013-08-17 13:55:12 -0400

    Keyphrase --> keyword for initial simplicity

--- models/models.js
+++ models/models.js
@@ -2,11 +2,11 @@ var mongoose = require('mongoose');
 
 //Empty string will be used to store data on all queries
 //This data is needed for Bayesian categorization
-var keyphraseSchema = mongoose.Schema({
-    phrase: String,
+var keywordSchema = mongoose.Schema({
+    word: String,
     services: [{name: String, count: Number}]
     total_count = Number
 });
 
-var Keyphrase = mongoose.model('Keyphrase', keyphraseSchema);
-exports.Keyphrase = Keyphrase;
+var Keyword = mongoose.model('Keyword', keywordSchema);
+exports.Keyword = Keyword;

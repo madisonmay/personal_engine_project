ca496e7060588cfe1f7c31ee761946dbecaad270
Madison May <madison.may@students.olin.edu>
2013-08-27 22:35:15 -0400

    Add user function

--- routes/connect.js
+++ routes/connect.js
@@ -1,1 +1,27 @@
-var rem = require('rem');
+var models = require('../models/models.js')
+var User = models.User;
+
+exports.register = function(req, res) {
+	res.render('register', {title: 'Register for spokes'});
+}
+
+exports.addUser = function(req, res) {
+	console.log(req.body);
+	User.findOne({gmail: gmail}, function(err, db_user) {
+		if (db_user) {
+			console.log("Account already exists...");
+			res.redirect('/');
+		} else {
+			var new_user = User({'gmail': req.body.gmail});
+			new_user.save(function(err, db_user) {
+			    if(err) {
+			        console.log("Error: ", err);
+			        res.send('/500');
+			    } else {
+			        req.session.user = db_user
+			        res.redirect('/');
+			    }
+			});
+		}
+	})
+}
